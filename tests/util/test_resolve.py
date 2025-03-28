76a9df4d8d4f0eae5b0e384bb6464df31eec220a
kuyugama <mail.kuyugama@gmail.com>
2025-03-28 21:43:03 +0200

    Add test for resolving dependency values by type Add example for resolving dependency values by type

--- tests/util/test_resolve.py
+++ tests/util/test_resolve.py
@@ -43,3 +43,29 @@ def test_resolve_async():
 
             if result.parameter_name == "arg":
                 assert result.value == 1
+
+
+def test_resolve_by_type():
+    class EventHandler:
+        pass
+
+    def dep():
+        pass
+
+    # Using from_ on type tells resolver that it should search value in scope by type, not parameter name
+    def func(arg: int, arg1: str, handler: from_(EventHandler)):
+        pass
+
+    event_handler = EventHandler()
+
+    for result in resolve({"arg": 1, "arg1": "value", "+1": event_handler}, scan(func), {}):
+        assert result.parameter_name in ("arg", "arg1", "handler")
+
+        if result.parameter_name == "arg1":
+            assert result.value == "value"
+
+        if result.parameter_name == "arg":
+            assert result.value == 1
+
+        if result.parameter_name == "handler":
+            assert result.value is event_handler
