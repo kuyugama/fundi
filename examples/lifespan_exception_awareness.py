fa1975ad3639b832231c037fbebf8cbeeae5a7b8
kuyugama <mail.kuyugama@gmail.com>
2025-04-21 07:27:13 +0300

    Add example that shows that lifespan dependency is aware about downstream exceptions

---
+++ examples/lifespan_exception_awareness.py
@@ -0,0 +1,35 @@
+from contextlib import ExitStack
+
+from fundi import from_, scan, inject, injection_trace
+
+
+def lifespan():
+    try:
+        yield
+        print("Injection happened successfully")
+    except ConnectionRefusedError as e:  # <== Lifespan dependency caught exception
+        print("exception happened on teardown:", e)
+        print("injection trace:")
+        trace = injection_trace(e)
+        while trace:
+            print(" ", trace.info.call, "with", trace.values)
+            trace = trace.origin
+
+        print()
+
+
+def require_random_animal(b=from_(lifespan)) -> str:
+    raise ConnectionRefusedError("Cannot connect to random.animal.com")  # <== Exception happened here
+
+
+def application(
+    animal: str = from_(require_random_animal),
+):
+    print("Animal:", animal)
+
+
+try:
+    with ExitStack() as stack:
+        inject({}, scan(application), stack)
+except ConnectionRefusedError:  # <== Lifespan dependency does not reraise exception, but it still goes downstream
+    print("ConnectionRefusedError happened on injection")
