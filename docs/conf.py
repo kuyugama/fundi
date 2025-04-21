fa41c0c83e0f0c12b3ae9a823f00cf819b8e4943
kuyugama <mail.kuyugama@gmail.com>
2025-04-21 08:03:12 +0300

    Bump version

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "FunDI"
 copyright = "2025, Kuyugama"
 author = "Kuyugama"
-release = "1.0.1"
+release = "1.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
